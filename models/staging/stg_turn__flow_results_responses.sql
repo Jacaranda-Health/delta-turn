83ed9973001624874f64daa50230dc2623e6db1e
Neshmwaniki <d.munene.mwaniki@gmail.com>
2026-07-13 15:01:06 +0300

    feat: initialise delta_turn dbt project with Turn staging layer

---
+++ models/staging/stg_turn__flow_results_responses.sql
@@ -0,0 +1,2 @@
+{{ config(materialized='view') }}
+select * from {{ source('turn', 'flow_results_responses') }}
---
+++ test_ch.py
@@ -0,0 +1,7 @@
+﻿import clickhouse_connect, getpass
+pw = getpass.getpass('Password: ')
+c = clickhouse_connect.get_client(
+    host='tplb1fkekn.eu-west-2.aws.clickhouse.cloud',
+    port=8443, username='jh_dna_dev', password=pw,
+    secure=True, verify=False)
+print(c.query('SELECT 1').result_rows)
